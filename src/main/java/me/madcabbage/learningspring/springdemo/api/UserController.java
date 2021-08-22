efe8e5281cd7db86e633aa842e744695b180e776
Justin <justin.t.mclean@gmail.com>
2021-08-21 18:34:21 -0600

    Add deteUserById() Add updateUser()

--- src/main/java/me/madcabbage/learningspring/springdemo/api/UserController.java
+++ src/main/java/me/madcabbage/learningspring/springdemo/api/UserController.java
@@ -28,9 +28,20 @@ public class UserController {
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+
     @GetMapping(path = "{id}")
     public User getUserById(@PathVariable("id") UUID id) {
         return userService.getUserById(id)
                 .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteUserById(@PathVariable("id") UUID id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateUser(@PathVariable("id") UUID id, @RequestBody User userToUpdate) {
+        userService.updateUser(id, userToUpdate);
     }
 }
