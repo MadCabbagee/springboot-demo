00e8f976347ee9df51b256d4aa69c0503eed27ad
Justin <justin.t.mclean@gmail.com>
2021-08-21 19:07:40 -0600

    Add @NotBlank annotation to String name;

--- src/main/java/me/madcabbage/learningspring/springdemo/model/User.java
+++ src/main/java/me/madcabbage/learningspring/springdemo/model/User.java
@@ -1,12 +1,15 @@
 package me.madcabbage.learningspring.springdemo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
 public class User {
 
     private final UUID id;
+    @NotBlank
     private final String name;
 
     public User(@JsonProperty("id") UUID id,
